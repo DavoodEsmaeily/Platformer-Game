96933b4ffbfc615d889f3349d626168809612730
DavoodEsmaeily0048@gmail.com <Davood123>
2023-05-29 22:36:21 +0330

    Start Game Project and implented State for PlayerIdle and PlayerMove

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,142 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Entity
+public class Player : MonoBehaviour 
 {
-    private float xInput;
-    //Clean Code with use of SerializeField Access to UI 
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
+    #region Components
+    public Animator Anim { get; private set; }
+    #endregion
 
+    #region States
+    public PlayerStateMachine StateMachine { get; private set; }
+    public PlayerIdleState IdleState { get; private set; }
+    public PlayerMoveState MoveState { get; private set; }
+    #endregion
 
-    [Header("Dash Info")]
-    [SerializeField] private float dashSpeed;
-    [SerializeField] private float dashDuration;
-    [SerializeField] private float dashTime;
-
-    [Header("Attack info")]
-    [SerializeField] private float comboTime = .3f;
-    [SerializeField] private float comboTimeWindow;
-    [SerializeField] private bool isAttacking;
-    [SerializeField] private int comboCounter;
-    [SerializeField] private float dashCooldown;
-    [SerializeField] private float dashCooldownTimer;
-
-    // Start is called before the first frame update
-    protected override void Start()
+    private void Awake()
     {
-        base.Start();
-    }
+        StateMachine = new PlayerStateMachine();
 
-   
-    // Update is called once per frame
-    protected override void Update()
-    {
-        base.Update();
+        IdleState = new PlayerIdleState(StateMachine, this, "idle");
 
-        Movement();
-        CheckInput();
-
-        dashTime -= Time.deltaTime;
-        comboTimeWindow -= Time.deltaTime;
-        dashCooldownTimer -= Time.deltaTime;
-
-
-
-        FlipController();
-        AnimatorController();
-    }
-
-    private void CheckInput()
-    {
-        xInput = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            DashAbility();
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            StartAttackEvent();
-        }
-    }
-
-    private void StartAttackEvent()
-    {
-        if (!isGrounded)
-            return;
-
-        if (comboTimeWindow < 0)
-            comboCounter = 0;
-
-        isAttacking = true;
-        comboTimeWindow = comboTime;
+        MoveState = new PlayerMoveState(StateMachine, this, "move");
     }
 
 
-    private void DashAbility()
+    private void Start()
     {
-        if(dashCooldownTimer < 0 && !isAttacking)  
-        {
-            dashCooldownTimer = dashCooldown;
-            dashTime = dashDuration;
-        }
+        Anim = GetComponentInChildren<Animator>();
+
+        StateMachine.Initialize(IdleState);
+     
+
     }
 
-    private void Movement()
+    private void Update()
     {
-        if(isAttacking)
-        {
-            rb.velocity = new Vector2(0, 0);
-        }
-       else if (dashTime > 0)
-            rb.velocity = new Vector2(facingDir * dashSpeed, 0);
-        else
-            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-    }
-
-    private void Jump()
-    {
-        if (isGrounded)
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-    }
-
-    public void AttackOver()
-    {
-        isAttacking = false;
-        comboCounter++;
-
-        if (comboCounter > 2)
-            comboCounter = 0;
-    }
-
-    private void AnimatorController()
-    {
-        // Animation Moving Player
-        bool isMoving = rb.velocity.x != 0;
-        anim.SetFloat("yVelocity", rb.velocity.y);
-
-        anim.SetBool("isMoving", isMoving);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isDashing", dashTime > 0);
-        anim.SetBool("isAttack", isAttacking);
-        anim.SetInteger("comboCounter", comboCounter);
-    }
-
-    private void FlipController()
-    {
-        if (rb.velocity.x > 0 && !flipReight)
-            Flip();
-        else if (rb.velocity.x < 0 && flipReight)
-            Flip();
-    }
-
-    protected override void OnDrawGizmos()
-    {
-        base.OnDrawGizmos();
+        StateMachine.CurrentState.Update(); 
     }
 
 }
